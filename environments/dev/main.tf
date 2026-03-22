2894bc634a3835aa325e01fdf77ce34498778f43
Lakshit Tyagi <lakshit175@gmail.com>
2026-03-22 16:57:34 +0530

    Added Environment Files

---
+++ environments/dev/main.tf
@@ -0,0 +1,25 @@
+module "networking" {
+  source = "../../modules/networking"
+
+  project_name      = var.project_name
+  environment       = var.environment
+  owner             = var.owner
+  instance_tenancy  = var.instance_tenancy
+  cidr_block        = var.cidr_block
+  availability_zone = var.availability_zone
+  subnet_public_cidr_block = var.subnet_public_cidr_block
+  subnet_private_cidr_block = var.subnet_private_cidr_block
+}
+
+module "compute" {
+  source = "../../modules/compute"
+
+  project_name = var.project_name
+  environment  = var.environment
+  owner        = var.owner
+
+  instance_type = var.instance_type
+  vpc_id    = module.networking.vpc_id
+  public_subnet_id   = module.networking.public_subnet_id
+  private_subnet_id  = module.networking.private_subnet_id
+}
---
+++ environments/dev/output.tf
@@ -0,0 +1,11 @@
+output "ec2_public_ips" {
+  value = module.compute.ec2_public_ips
+}
+
+output "master_public_ip" {
+  value = module.compute.master_public_ip
+}
+
+output "argocd_url" {
+  value = module.compute.argocd_url
+}
